b29d83aaa813c957b734f47a3833a6fc1523232c
Laslaa Mohammed <mohamed.laslaa@gmail.com>
2020-03-14 13:36:46 +0100

    fixe collision issue

--- game/script.js
+++ game/script.js
@@ -9,75 +9,183 @@ window.onload = function() {
   let guerriertitle = document.querySelector(".guerriertitle");
   let magetitle = document.querySelector(".magetitle");
   let voleurtitle = document.querySelector(".voleurtitle");
+  let errormessage = document.querySelector(".errormessage");
 
-  //   console.log(window.innerHeight);
-  //   console.log(window.innerWidth);
-
-  const Personnage = function(puissance, name, image) {
+  const Personnage = function(puissance, defense, name, image) {
     this.levelLife = 100;
     this.puissance = puissance;
+    this.defense = defense;
     this.name = name;
     this.image = image;
+    this.collision = false;
 
     this.coup1 = function(victime) {
-      return (victime.levelLife = victime.levelLife - this.puissance);
+      return (victime.levelLife =
+        victime.levelLife - this.puissance + victime.defense);
     };
 
     this.coup2 = function(victime) {
-      return (victime.levelLife = victime.levelLife - this.puissance * 2);
+      return (victime.levelLife =
+        victime.levelLife - this.puissance * 2 + victime.defense);
     };
 
     this.coup3 = function(victime) {
-      return (victime.levelLife = victime.levelLife - this.puissance * 3);
+      return (victime.levelLife =
+        victime.levelLife - this.puissance * 3 + victime.defense);
     };
 
-    this.move = function(key) {
+    this.errorMessage = function() {
+      errormessage.textContent = "Mouvement non autorisée";
+      return false;
+    };
+
+    this.detectCollisionTop = function(arr) {
+      // write all case possible
+      //move top
+      this.collision = false;
+      arr.forEach(e => {
+        if (
+          this.image.getBoundingClientRect().top >
+            e.image.getBoundingClientRect().bottom &&
+          this.image.getBoundingClientRect().left <
+            e.image.getBoundingClientRect().right &&
+          this.image.getBoundingClientRect().right >
+            e.image.getBoundingClientRect().left
+        ) {
+          if (
+            this.image.getBoundingClientRect().top - 100 <
+            e.image.getBoundingClientRect().bottom
+          ) {
+            console.log("collision top ", this.name, " avec ", e.name);
+            return (this.collision = true);
+          }
+        }
+      });
+    };
+
+    this.detectCollisionBottom = function(arr) {
+      // write all case possible
+      //move bottom
+      this.collision = false;
+      arr.forEach(e => {
+        if (
+          this.image.getBoundingClientRect().bottom <
+            e.image.getBoundingClientRect().top &&
+          this.image.getBoundingClientRect().left <
+            e.image.getBoundingClientRect().right &&
+          this.image.getBoundingClientRect().right >
+            e.image.getBoundingClientRect().left
+        ) {
+          if (
+            this.image.getBoundingClientRect().bottom + 100 >
+            e.image.getBoundingClientRect().top
+          ) {
+            console.log("collision bottom", this.name, " avec ", e.name);
+            return (this.collision = true);
+          }
+        }
+      });
+    };
+
+    this.detectCollisionLeft = function(arr) {
+      // write all case possible
+      //move left
+      this.collision = false;
+      arr.forEach(e => {
+        if (
+          this.image.getBoundingClientRect().left >
+            e.image.getBoundingClientRect().right &&
+          this.image.getBoundingClientRect().top <
+            e.image.getBoundingClientRect().bottom &&
+          this.image.getBoundingClientRect().bottom >
+            e.image.getBoundingClientRect().top
+        ) {
+          if (
+            this.image.getBoundingClientRect().left - 100 <
+            e.image.getBoundingClientRect().right
+          ) {
+            console.log("collision left", this.name, " avec ", e.name);
+            return (this.collision = true);
+          }
+        }
+      });
+    };
+
+    this.detectCollisionRight = function(arr) {
+      // write all case possible
+      //move right
+      this.collision = false;
+      arr.forEach(e => {
+        if (
+          this.image.getBoundingClientRect().right <
+            e.image.getBoundingClientRect().left &&
+          this.image.getBoundingClientRect().top <
+            e.image.getBoundingClientRect().bottom &&
+          this.image.getBoundingClientRect().bottom >
+            e.image.getBoundingClientRect().top
+        ) {
+          if (
+            this.image.getBoundingClientRect().right + 100 >
+            e.image.getBoundingClientRect().left
+          ) {
+            console.log("collision right", this.name, " avec ", e.name);
+            return (this.collision = true);
+          }
+        }
+      });
+    };
+
+    this.move = function(key, arr) {
       switch (key) {
         case 37:
-          this.moveLeft();
-          break;
+          this.detectCollisionLeft(arr);
+          return this.moveLeft();
         case 38:
-          this.moveTop();
-          break;
+          this.detectCollisionTop(arr);
+          return this.moveTop();
         case 39:
-          this.moveRight();
-          break;
+          this.detectCollisionRight(arr);
+          return this.moveRight();
         case 40:
-          this.moveBottom();
-          break;
+          this.detectCollisionBottom(arr);
+          return this.moveBottom();
       }
     };
 
     this.moveLeft = function() {
-      if (this.image.offsetLeft - 50 < 0) return false;
-      this.image.style.left = this.image.offsetLeft - 50 + "px";
+      if (this.image.offsetLeft - 100 < 0 || this.collision)
+        return this.errorMessage();
+      return (this.image.style.left = this.image.offsetLeft - 100 + "px");
     };
 
     this.moveTop = function() {
       if (
-        this.image.offsetTop - 50 <
-        bartitle.offsetTop + bartitle.offsetHeight
+        this.image.offsetTop - 100 <
+          bartitle.offsetTop + bartitle.offsetHeight ||
+        this.collision
       )
-        return false;
-      this.image.style.top = this.image.offsetTop - 50 + "px";
+        return this.errorMessage();
+      return (this.image.style.top = this.image.offsetTop - 100 + "px");
     };
 
     this.moveRight = function() {
       if (
         this.image.offsetLeft + this.image.offsetWidth >
-        window.innerWidth - 50
+          window.innerWidth - 100 ||
+        this.collision
       )
-        return false;
-      this.image.style.left = this.image.offsetLeft + 50 + "px";
+        return this.errorMessage();
+      return (this.image.style.left = this.image.offsetLeft + 100 + "px");
     };
 
     this.moveBottom = function() {
       if (
         this.image.offsetTop + this.image.offsetHeight >
-        window.innerHeight - 50
+          window.innerHeight - 100 ||
+        this.collision
       )
-        return false;
-      this.image.style.top = this.image.offsetTop + 50 + "px";
+        return this.errorMessage();
+      return (this.image.style.top = this.image.offsetTop + 100 + "px");
     };
   };
 
@@ -91,11 +199,11 @@ window.onload = function() {
     voleurtitle.style.border = "2px solid green";
   }
 
-  let guerrier = new Personnage(5, "Guerrier", imgguerrier);
+  let guerrier = new Personnage(8, 2, "Guerrier", imgguerrier);
 
-  let mage = new Personnage(2, "Mage", imgmage);
+  let mage = new Personnage(6, 5, "Mage", imgmage);
 
-  let voleur = new Personnage(2, "Voleur", imgvoleur);
+  let voleur = new Personnage(5, 7, "Voleur", imgvoleur);
 
   guerrierscore.textContent = guerrier.levelLife;
   magescore.textContent = mage.levelLife;
@@ -114,28 +222,28 @@ window.onload = function() {
 
   document.addEventListener("keydown", function(e) {
     if (onhand == 1) {
-      imgguerrier.style.zIndex = 1;
-      guerrier.move(e.keyCode);
-      getstyleborder(magetitle);
-      imgguerrier.style.zIndex = 0;
-      onhand = 2;
-      return false;
+      errormessage.textContent = " ";
+      if (guerrier.move(e.keyCode, [mage, voleur])) {
+        getstyleborder(magetitle);
+        onhand = 2;
+        return false;
+      }
     }
     if (onhand == 2) {
-      imgmage.style.zIndex = 1;
-      mage.move(e.keyCode);
-      getstyleborder(voleurtitle);
-      imgmage.style.zIndex = 0;
-      onhand = 3;
-      return false;
+      errormessage.textContent = " ";
+      if (mage.move(e.keyCode, [guerrier, voleur])) {
+        getstyleborder(voleurtitle);
+        onhand = 3;
+        return false;
+      }
     }
     if (onhand == 3) {
-      imgvoleur.style.zIndex = 1;
-      voleur.move(e.keyCode);
-      getstyleborder(guerriertitle);
-      imgvoleur.style.zIndex = 0;
-      onhand = 1;
-      return false;
+      errormessage.textContent = " ";
+      if (voleur.move(e.keyCode, [mage, guerrier])) {
+        getstyleborder(guerriertitle);
+        onhand = 1;
+        return false;
+      }
     }
   });
 };
